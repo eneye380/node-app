7a2bedf3f22a82e6ce15ed917771797cd6b686c6
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2020-10-29 15:59:34 +0000

    background workers|logging to files commit

--- index.js
+++ index.js
@@ -1,118 +1,29 @@
 /*
- * PRIMARY FILE FOR THE API
+ * Primary file for API
  *
  */
 
 // Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+// Declare the app
+var app = {};
 
-//@TODO GET RID OF THIS
-helpers.sendTwilioSms('8051185104','Hello World!', function(err){
-    console.log('this was the error',err);
-});
+// Init function
+app.init = function(){
 
-// Instantiating the HTTP Server
-var httpServer = http.createServer(function (req, res) {
-    unifiedServer(req,res);
-});
+  // Start the server
+  server.init();
 
-// Start the server
-httpServer.listen(config.httpPort, function () {
-    console.log(`The server is listening on port ${config.httpPort}`);
-});
+  // Start the workers
+  workers.init();
 
-//Instantiate the HTTPS server
-var httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
 };
-var httpsServer = https.createServer(httpsServerOptions,function (req, res) {
-    unifiedServer(req,res);
-});
 
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, function () {
-    console.log(`The server is listening on port ${config.httpsPort}`);
-});
+// Self executing
+app.init();
 
 
-// All the server logic for both the http and https server
-var unifiedServer = function(req, res){
-     //Get the URL and parse it
-     var parsedUrl = url.parse(req.url, true);
-
-     // Get the path
-     var path = parsedUrl.pathname;
-     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
- 
-     // Get the query string as an object
-     var queryStringObject = parsedUrl.query;
- 
-     // Get the HTTP Method
-     var method = req.method.toLowerCase();
- 
- 
-     // Get the headers as an object
-     var headers = req.headers;
- 
-     // Get the payload, if any
-     var decoder = new StringDecoder('utf-8');
-     var buffer = '';
-     req.on('data', function (data) {
-         buffer += decoder.write(data);
-     });
- 
-     req.on('end', function () {
-         buffer += decoder.end();
- 
-         // Choose the handler this request should go to. If one is not found, use the notfound handler.
-         var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notfound;
- 
-         // Construct the data object to send to the handler
-         var data = {
-             'trimmedPath': trimmedPath,
-             'queryStringObject': queryStringObject,
-             'method': method,
-             'headers': headers,
-             'payload': helpers.parseJsonToObject(buffer)
-         };
- 
-         // Route the request to the handler specified in the router
-         chosenHandler(data, function (statusCode, payload) {
-             // Use the status code called back by the handler, or default to 200
-             statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
- 
-             // Use the payload called back by the handler, or default to an empty object
-             payload = typeof (payload) == 'object' ? payload : {};
- 
-             // Convert the payload to a string
-             var payloadString = JSON.stringify(payload);
- 
-             // Return the response
-             res.setHeader('Content-Type', 'application/json');
-             res.writeHead(statusCode);
-             res.end(payloadString);
- 
-             // Log the request path
-             console.log('Returning this response: ', statusCode, payloadString);
-         });
- 
- 
-     });
-}
-
-// Define a request router
-var router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks,
-}
+// Export the app
+module.exports = app;
